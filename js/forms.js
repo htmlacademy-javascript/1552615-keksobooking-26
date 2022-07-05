c411150a42847c334de21afcd3faf7d6a294dbeb
KIlnur <kilnur1988@rambler.ru>
2022-07-05 17:17:01 +0500

    Добавил деактивацию и активацию слайдера

--- js/forms.js
+++ js/forms.js
@@ -2,12 +2,14 @@ import { setDisabledAttr, removeDisabledAttr } from './util.js';
 
 const adFrom = document.querySelector('.ad-form');
 const adFormElements = adFrom.querySelectorAll('.ad-form__element');
+const adSlider = adForm.querySelector('.ad-form__slider');
 const mapFilter = document.querySelector('.map__filters');
 const mapFilters = mapFilter.querySelectorAll('.map__filter');
 const mapFeatures = mapFilter.querySelector('.map__features');
 
 const disableForms = () => {
   adFrom.classList.add('ad-form--disabled');
+  adSlider.setAttribute('disabled', 'disabled');
   mapFeatures.setAttribute('disabled', 'disabled');
   mapFilter.classList.add('map__filters--disabled');
   setDisabledAttr(adFormElements);
@@ -16,6 +18,7 @@ const disableForms = () => {
 
 const activateForms = () => {
   adFrom.classList.remove('ad-form--disabled');
+  adSlider.removeAttribute('disabled', 'disabled');
   mapFeatures.removeAttribute('disabled', 'disabled');
   mapFilter.classList.remove('map__filters--disabled');
   removeDisabledAttr(adFormElements);
