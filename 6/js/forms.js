76411c8c15c1e040ffa429ad84bd43deab063d7f
Keks <keks@htmlacademy.ru>
2022-07-05 18:38:26 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/forms.js
@@ -0,0 +1,30 @@
+import { setDisabledAttr, removeDisabledAttr } from './util.js';
+
+const adForm = document.querySelector('.ad-form');
+const adFormElements = adForm.querySelectorAll('.ad-form__element');
+const adSlider = adForm.querySelector('.ad-form__slider');
+const mapFilter = document.querySelector('.map__filters');
+const mapFilters = mapFilter.querySelectorAll('.map__filter');
+const mapFeatures = mapFilter.querySelector('.map__features');
+
+const disableForms = () => {
+  adForm.classList.add('ad-form--disabled');
+  adSlider.setAttribute('disabled', 'disabled');
+  mapFeatures.setAttribute('disabled', 'disabled');
+  mapFilter.classList.add('map__filters--disabled');
+  setDisabledAttr(adFormElements);
+  setDisabledAttr(mapFilters);
+};
+
+const activateForms = () => {
+  adForm.classList.remove('ad-form--disabled');
+  adSlider.removeAttribute('disabled', 'disabled');
+  mapFeatures.removeAttribute('disabled', 'disabled');
+  mapFilter.classList.remove('map__filters--disabled');
+  removeDisabledAttr(adFormElements);
+  removeDisabledAttr(mapFilters);
+};
+
+disableForms();
+
+activateForms();
---
+++ 6/js/main.js
@@ -0,0 +1,4 @@
+import './data.js';
+import './card-popup.js';
+import './forms.js';
+
